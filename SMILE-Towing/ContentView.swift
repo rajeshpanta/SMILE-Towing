e2a140d47c168cc37d4a0ad86b63ea3ed6cd1831
rajeshpanta <rpanta@sfsu.edu>
2024-12-04 07:23:13 -0800

    Initial Commit

---
+++ SMILE-Towing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SMILE-Towing
+//
+//  Created by Rajesh Panta on 12/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SMILE-Towing/SMILE_TowingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMILE_TowingApp.swift
+//  SMILE-Towing
+//
+//  Created by Rajesh Panta on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SMILE_TowingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
